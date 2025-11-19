fa926b20af89d47050921251673692752d01f969
June Smith <smithjun@oregonstate.edu>
2025-11-19 01:52:09 -0800

    Error page fix

--- src/app/login/actions.ts
+++ src/app/login/actions.ts
@@ -9,7 +9,6 @@ export async function login(formData: FormData) {
   const supabase = await createClient()
 
   // type-casting here for convenience
-  // in practice, you should validate your inputs
   const data = {
     email: formData.get('email') as string,
     password: formData.get('password') as string,
@@ -29,12 +28,11 @@ export async function signup(formData: FormData) {
   const supabase = await createClient()
 
   // type-casting here for convenience
-  // in practice, you should validate your inputs
   const data = {
     email: formData.get('email') as string,
     password: formData.get('password') as string,
   }
-  
+
   const { error } = await supabase.auth.signUp(data)
 
   if (error) {
